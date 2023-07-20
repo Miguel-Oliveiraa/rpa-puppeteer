09709315351b0b6ae200e7a829f6b31d50f7e51f
Miguel Oliveira <miguellgomes64@gmail.com>
2023-07-19 22:42:54 -0300

    code to test api

---
+++ index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+// Função para executar a solicitação HTTP com um nome específico
+async function fetchDataFromAPI(nome) {
+  try {
+    const response = await axios.get(`http://localhost:3000/api/olx-data/${nome}`);
+    console.log(response.data);
+  } catch (error) {
+    console.error('Erro ao acessar a API:', error);
+  }
+}
+
+// Chame a função para obter os dados do OLX por meio da API, fornecendo um nome específico
+const nomePesquisa = 'jardim primavera'; // Substitua pelo nome que você deseja pesquisar
+fetchDataFromAPI(nomePesquisa);
